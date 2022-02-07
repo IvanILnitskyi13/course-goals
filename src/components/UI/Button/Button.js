8ca0048c70e2af63160a4b1a7e3ebc5ac55a46d7
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2022-02-07 21:23:37 +0100

    styled-componemts package

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,17 @@
-//import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -20,13 +24,5 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
 
 export default Button;
